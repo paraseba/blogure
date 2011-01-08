aedd8c566615bb1b5d7dc5767f0aab8f5efac5b1
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-01-08 16:33:59 -0200

    receive config dirs

--- src/blogure/core.clj
+++ src/blogure/core.clj
@@ -1,4 +1,4 @@
-(ns unblogate.core
+(ns blogure.core
   (:import java.util.Date)
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
@@ -39,22 +39,19 @@
   {:now (Date.)})
 
 
-(defn post-processor [site-data]
-  (fn [post]
-    (reduce
-      (fn [res [k v]]
-        (assoc res k (v site-data)))
-      {} post)))
+(defn process [site-data post]
+  (reduce
+    (fn [res [k v]]
+      (assoc res k (v site-data)))
+    {} post))
 
 (defn process-posts [posts site-data]
-  (let [proc (post-processor site-data)]
-    (sort-by #(-> % :publish-date .getTime -)
-             (map proc posts))))
+  (sort-by #(-> % :publish-date .getTime -)
+           (pmap (partial process site-data) posts)))
 
 (defn process-pages [pages posts site-data]
-  (let [site-data (assoc site-data :posts posts)
-        proc (post-processor site-data)]
-    (map proc pages)))
+  (let [site-data (assoc site-data :posts posts)]
+    (pmap (partial process site-data) pages)))
 
 (defn url-to-path [url]
   (string/replace (str "site/" url) "-" "_"))
@@ -64,4 +61,16 @@
         path (url-to-path url)]
     (.mkdirs (.getParentFile (io/file path)))
     (spit path content)))
+
+(defn generate [config]
+  (let [{:keys [source-dir dest-dir]} config
+        posts-dir (file source-dir "posts")
+        pages-dir (file source-dir "public/html")
+        data (site-data)
+        posts (published-posts (read-posts posts-dir))
+        posts (process-posts posts data)
+        pages (process-pages (read-pages pages-dir) posts data)]
+    (doall (map save-html posts))
+    (doall (map save-html pages)))
+  (shutdown-agents))
 
